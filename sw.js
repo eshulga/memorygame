a5477e411b59955f6262f0bd3fd742446451e0b9
eshulga <ewgenij.shulga@gmail.com>
2018-02-02 11:22:41 +0200

    fix path

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
 importScripts('sw-toolbox.js');
 toolbox.precache(['index.html','css/style.css', 'js/main.js']);
-toolbox.router.get('/images/*', toolbox.cacheFirst);
+toolbox.router.get('/ico/*', toolbox.cacheFirst);
 toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
